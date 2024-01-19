d97aa2640246b8e5aa9450d744ac2aadbc58373b
Givari Hertz <h3rtz@h3rtz.kali>
2024-01-20 00:32:14 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# praktek_script.py
+
+from termcolor import colored
+
+def main():
+    print(colored("Selamat anda berhasil mem-praktek-kan Git Clone", 'red', attrs=['bold']))
+
+if __name__ == "__main__":
+    main()
